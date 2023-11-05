5cf2526e4ea95e89dda4ba34134cde2f6dc35648
Alberto Prada <albertojprada10@gmail.com>
2023-11-05 12:01:42 +0000

    Add RecipeTabView.swift as the starting point of the App

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Alberto Jesús Prada Parmera on 05/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeTabView()
+}
