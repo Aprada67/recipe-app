927765fedc80debfec062177178511daf6b4278f
Alberto Prada <albertojprada10@gmail.com>
2023-11-19 09:22:19 +0000

    Changes the default font for the Avenir font of the entire app

---
+++ Recipe List App/Views/RecipeHighlights.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeHighlights.swift
+//  Recipe List App
+//
+//  Created by Alberto Jesús Prada Parmera on 18/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeHighlights: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeHighlights()
+}
